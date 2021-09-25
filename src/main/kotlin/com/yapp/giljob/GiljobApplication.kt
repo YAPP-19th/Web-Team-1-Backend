42925b56cec26869005424a3864ffae14d5482ce
ayoung0073 <ayong0310@naver.com>
2021-09-25 18:49:26 +0900

    [GILJOB-11] 프로젝트 생성

---
+++ src/main/kotlin/com/yapp/giljob/GiljobApplication.kt
@@ -0,0 +1,11 @@
+package com.yapp.giljob
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GiljobApplication
+
+fun main(args: Array<String>) {
+    runApplication<GiljobApplication>(*args)
+}
---
+++ src/test/kotlin/com/yapp/giljob/GiljobApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yapp.giljob
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GiljobApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
