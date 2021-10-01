fcaac706334885ed443ca8ecbceeb09e80340c82
ayoung0073 <ayong0310@naver.com>
2021-10-01 23:53:55 +0900

    [GILJOB-12] Log 클래스 추가

--- src/main/kotlin/com/yapp/giljob/common/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/yapp/giljob/common/exception/GlobalExceptionHandler.kt
@@ -1,8 +1,7 @@
 package com.yapp.giljob.common.exception
 
 import com.yapp.giljob.common.dto.BaseResponse
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import com.yapp.giljob.common.util.Log
 import org.springframework.http.HttpStatus
 
 import org.springframework.http.ResponseEntity
@@ -12,17 +11,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 @RestControllerAdvice
 class GlobalExceptionHandler {
 
-    private val log : Logger get() = LoggerFactory.getLogger(this::class.java)
-
     @ExceptionHandler(BusinessException::class)
-    fun handleBaseException(exception: BusinessException): ResponseEntity<BaseResponse<Unit>> {
-        log.error(exception.message)
-        return ResponseEntity.status(exception.exceptionCode.status).body(BaseResponse.exception(exception.exceptionCode))
+    fun handleBaseException(e: BusinessException): ResponseEntity<BaseResponse<Unit>> {
+        Log.error(e.exceptionCode.message)
+        return ResponseEntity.status(e.exceptionCode.status).body(BaseResponse.exception(e.exceptionCode))
     }
 
     @ExceptionHandler(RuntimeException::class)
-    fun handleUnexpectedException(exception: RuntimeException): ResponseEntity<BaseResponse<Unit>> {
-        log.error("RuntimeException")
+    fun handleUnexpectedException(e: RuntimeException): ResponseEntity<BaseResponse<Unit>> {
+        Log.error(e.message)
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(BaseResponse.exception(ExceptionCode.INTERNAL_SERVER_ERROR))
     }
 
---
+++ src/main/kotlin/com/yapp/giljob/common/util/Log.kt
@@ -0,0 +1,14 @@
+package com.yapp.giljob.common.util
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class Log {
+    companion object {
+        private val log : Logger get() = LoggerFactory.getLogger(this::class.java)
+
+        fun error(message: String?) {
+            log.error(message)
+        }
+    }
+}
