dc3381da467ffde2f179d33c82691ea37969839d
ayoung0073 <ayong0310@naver.com>
2021-11-10 01:40:44 +0900

    [GILJOB-37] 태그 저장 관련 테스트 추가

---
+++ src/test/kotlin/com/yapp/giljob/domain/quest/application/QuestServiceTest.kt
@@ -0,0 +1,81 @@
+package com.yapp.giljob.domain.quest.application
+
+import com.yapp.giljob.domain.quest.dao.QuestRepository
+import com.yapp.giljob.domain.tag.dao.TagRepository
+import com.yapp.giljob.domain.tag.domain.QuestTag
+import com.yapp.giljob.domain.tag.domain.QuestTagPK
+import com.yapp.giljob.domain.tag.domain.Tag
+import com.yapp.giljob.global.common.domain.EntityFactory
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class QuestServiceTest {
+
+    private lateinit var questService: QuestService
+
+    @MockK
+    private lateinit var questRepository: QuestRepository
+
+    @MockK
+    private lateinit var tagRepository: TagRepository
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+        questService = QuestService(questRepository, tagRepository)
+    }
+
+    @Nested
+    inner class QuestTag {
+        private val questWithoutId = EntityFactory.testQuestWithoutId()
+        private val questWithId = EntityFactory.testQuestWithId()
+
+        private lateinit var tagList: MutableList<Tag>
+
+        private val savedTagName = "tag1"
+        private val savedTag = Tag(1L, "tag1")
+
+        private val newTagName = "tag2"
+        private val newTag = Tag(2L, "tag2")
+
+        @Test
+        fun `이미 저장된 태그 저장되는지 테스트`() {
+            // given
+            tagList = mutableListOf(Tag(name = savedTagName))
+            questWithId.tagList = mutableListOf(QuestTag(QuestTagPK(questWithId.id, savedTag.id)))
+
+            every { tagRepository.findByName(savedTagName) } returns savedTag
+            every { questRepository.save(any()) } returns questWithId
+
+            // when
+            val savedQuest = questService.saveQuest(questWithoutId, tagList)
+
+            // them
+            assertEquals(savedTag.id, savedQuest.tagList[0].id.tagId)
+            assertEquals(questWithId.id, savedQuest.tagList[0].id.questId)
+        }
+
+        @Test
+        fun `새로 저장된 태그 저장되는지 테스트`() {
+            // given
+            tagList = mutableListOf(Tag(name = newTagName))
+            questWithId.tagList = mutableListOf(QuestTag(QuestTagPK(questWithId.id, newTag.id)))
+
+            every { tagRepository.findByName(newTagName) } returns null
+            every { tagRepository.save(any()) } returns newTag
+            every { questRepository.save(any()) } returns questWithId
+
+            // when
+            val savedQuest = questService.saveQuest(questWithoutId, tagList)
+
+            // then
+            assertEquals(newTag.id, savedQuest.tagList[0].id.tagId)
+            assertEquals(questWithId.id, savedQuest.tagList[0].id.questId)
+        }
+    }
+}
