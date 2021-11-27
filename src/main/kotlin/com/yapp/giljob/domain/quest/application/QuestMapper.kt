02d21509f29527df534797901daf67eccea6617a
ayoung0073 <ayong0310@naver.com>
2021-11-27 20:00:39 +0900

    [GILJOB-77] 피드백 반영

--- src/main/kotlin/com/yapp/giljob/domain/quest/application/QuestMapper.kt
+++ src/main/kotlin/com/yapp/giljob/domain/quest/application/QuestMapper.kt
@@ -12,23 +12,23 @@ import org.mapstruct.Mappings
 @Mapper(componentModel = "spring", uses = [UserMapper::class])
 interface QuestMapper {
     @Mappings(
-        Mapping(target = "id", source = "quest.quest.id"),
-        Mapping(target = "name", source = "quest.quest.name"),
-        Mapping(target = "position", source = "quest.quest.position"),
-        Mapping(target = "difficulty", source = "quest.quest.difficulty"),
-        Mapping(target = "thumbnail", source = "quest.quest.thumbnail"),
+        Mapping(target = "id", source = "questSupportVo.quest.id"),
+        Mapping(target = "name", source = "questSupportVo.quest.name"),
+        Mapping(target = "position", source = "questSupportVo.quest.position"),
+        Mapping(target = "difficulty", source = "questSupportVo.quest.difficulty"),
+        Mapping(target = "thumbnail", source = "questSupportVo.quest.thumbnail"),
         Mapping(target = "user", source = "user")
     )
-    fun toDto(quest: QuestSupportVo, user: UserSubDto): QuestResponseDto
+    fun toDto(questSupportVo: QuestSupportVo, user: UserSubDto): QuestResponseDto
 
     @Mappings(
-        Mapping(target = "id", source = "quest.quest.id"),
-        Mapping(target = "name", source = "quest.quest.name"),
-        Mapping(target = "position", source = "quest.quest.position"),
-        Mapping(target = "difficulty", source = "quest.quest.difficulty"),
-        Mapping(target = "thumbnail", source = "quest.quest.thumbnail"),
+        Mapping(target = "id", source = "questSupportVo.quest.id"),
+        Mapping(target = "name", source = "questSupportVo.quest.name"),
+        Mapping(target = "position", source = "questSupportVo.quest.position"),
+        Mapping(target = "difficulty", source = "questSupportVo.quest.difficulty"),
+        Mapping(target = "thumbnail", source = "questSupportVo.quest.thumbnail"),
         Mapping(target = "user", source = "user"),
         Mapping(target = "progress", source = "progress"),
     )
-    fun toDto(quest: QuestSupportVo, user: UserSubDto, progress: Int): QuestByParticipantResponseDto
+    fun toDto(questSupportVo: QuestSupportVo, user: UserSubDto, progress: Int): QuestByParticipantResponseDto
 }
