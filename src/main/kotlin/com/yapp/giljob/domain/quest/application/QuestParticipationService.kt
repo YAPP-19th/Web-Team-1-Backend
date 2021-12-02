fcedd09e7013636911d8b7fa71a82634b02a810c
ayoung0073 <ayong0310@naver.com>
2021-12-02 23:49:25 +0900

    [GILJOB-86] 퀘스트 완료 api

--- src/main/kotlin/com/yapp/giljob/domain/quest/application/QuestParticipationService.kt
+++ src/main/kotlin/com/yapp/giljob/domain/quest/application/QuestParticipationService.kt
@@ -5,16 +5,20 @@ import com.yapp.giljob.domain.quest.dao.QuestRepository
 import com.yapp.giljob.domain.quest.domain.QuestParticipation
 import com.yapp.giljob.domain.quest.domain.QuestParticipationPK
 import com.yapp.giljob.domain.quest.dto.response.QuestCountResponseDto
+import com.yapp.giljob.domain.subquest.application.SubQuestService
 import com.yapp.giljob.domain.user.domain.User
 import com.yapp.giljob.global.error.ErrorCode
 import com.yapp.giljob.global.error.exception.BusinessException
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class QuestParticipationService(
     private val questRepository: QuestRepository,
-    private val questParticipationRepository: QuestParticipationRepository
+    private val questParticipationRepository: QuestParticipationRepository,
+
+    private val subQuestService: SubQuestService
 ) {
     @Transactional
     fun participateQuest(questId: Long, user: User) {
@@ -32,6 +36,26 @@ class QuestParticipationService(
         onProgressQuestCount = getOnProgressQuestCount(),
         totalParticipantCount = getQuestParticipantCount()
     )
+
+
+    @Transactional
+    fun completeQuest(questId: Long, user: User) {
+        val questParticipation = questParticipationRepository.findByIdOrNull(QuestParticipationPK(user.id!!, questId))
+            ?: throw BusinessException(ErrorCode.ENTITY_NOT_FOUND)
+        validateCompletedQuest(questParticipation, questId, user.id!!)
+        questParticipation.isCompleted = true
+        // TODO 유저 경험치 획득
+    }
+
+    private fun validateCompletedQuest(questParticipation: QuestParticipation, questId: Long, userId: Long) {
+        if (questParticipation.isCompleted) {
+            throw BusinessException(ErrorCode.ALREADY_COMPLETED_QUEST)
+        }
+        val completedSubQuestCount = subQuestService.countCompletedSubQuest(questId, userId)
+        if (questParticipation.quest.subQuestList.size != completedSubQuestCount) {
+            throw BusinessException(ErrorCode.NOT_COMPLETED_QUEST)
+        }
+    }
 
     private fun getOnProgressQuestCount() = questParticipationRepository.countQuests()
 
--- src/main/kotlin/com/yapp/giljob/domain/subquest/application/SubQuestService.kt
+++ src/main/kotlin/com/yapp/giljob/domain/subquest/application/SubQuestService.kt
@@ -1,14 +1,22 @@
 package com.yapp.giljob.domain.subquest.application
 
 import com.yapp.giljob.domain.quest.domain.Quest
+import com.yapp.giljob.domain.subquest.dao.SubQuestParticipationRepository
+import com.yapp.giljob.domain.subquest.dao.SubQuestRepository
 import com.yapp.giljob.domain.subquest.domain.SubQuest
 import com.yapp.giljob.domain.subquest.dto.request.SubQuestRequestDto
+import com.yapp.giljob.domain.user.domain.User
 import org.springframework.stereotype.Service
 
 @Service
-class SubQuestService {
+class SubQuestService(
+    private val subQuestParticipationRepository: SubQuestParticipationRepository
+) {
     fun convertToSubQuestList(quest: Quest, subQuestRequestList: List<SubQuestRequestDto>) =
         subQuestRequestList.map {
             SubQuest(quest = quest, name = it.name)
         }
+
+    fun countCompletedSubQuest(questId: Long, participantId: Long) =
+        subQuestParticipationRepository.countByQuestIdAndParticipantIdAndIsCompletedTrue(questId, participantId)
 }
