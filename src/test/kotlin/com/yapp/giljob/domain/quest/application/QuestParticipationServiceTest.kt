fb15ba76d92aab389cef388b3327bde2d09f04d4
ayoung0073 <ayong0310@naver.com>
2021-12-03 00:07:35 +0900

    [GILJOB-86] 퀘스트 완료 테스트 추가

--- src/test/kotlin/com/yapp/giljob/domain/quest/application/QuestParticipationServiceTest.kt
+++ src/test/kotlin/com/yapp/giljob/domain/quest/application/QuestParticipationServiceTest.kt
@@ -3,6 +3,7 @@ package com.yapp.giljob.domain.quest.application
 import com.yapp.giljob.domain.position.domain.Position
 import com.yapp.giljob.domain.quest.dao.QuestParticipationRepository
 import com.yapp.giljob.domain.quest.dao.QuestRepository
+import com.yapp.giljob.domain.subquest.application.SubQuestService
 import com.yapp.giljob.domain.user.domain.User
 import com.yapp.giljob.global.common.domain.EntityFactory
 import com.yapp.giljob.global.error.ErrorCode
@@ -13,10 +14,11 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.verify
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.data.repository.findByIdOrNull
 
-class sQuestParticipationServiceTest {
+class QuestParticipationServiceTest {
 
     private lateinit var questParticipationService: QuestParticipationService
 
@@ -26,71 +28,145 @@ class sQuestParticipationServiceTest {
     @MockK
     private lateinit var questParticipationRepository: QuestParticipationRepository
 
+    @MockK
+    private lateinit var subQuestService: SubQuestService
+
     @BeforeEach
     fun setUp() {
         MockKAnnotations.init(this)
-        questParticipationService = QuestParticipationService(questRepository, questParticipationRepository)
+        questParticipationService = QuestParticipationService(questRepository, questParticipationRepository, subQuestService)
     }
 
     private val questId = 1L
     private val user = EntityFactory.testUser()
     private val quest = EntityFactory.testQuest()
 
-    @Test
-    fun `존재하지 않은 퀘스트에 참여하면 예외가 발생한다`() {
-        // given
-        every { questRepository.findByIdOrNull(any()) } returns null
+    @Nested
+    inner class QuestParticipation {
 
-        // when
-        val exception =
-            assertThrows(BusinessException::class.java) {
-                questParticipationService.participateQuest(questId, user)
-            }
+        @Test
+        fun `존재하지 않은 퀘스트에 참여하면 예외가 발생한다`() {
+            // given
+            every { questRepository.findByIdOrNull(any()) } returns null
 
-        // then
-        assertEquals(ErrorCode.ENTITY_NOT_FOUND, exception.errorCode)
+            // when
+            val exception =
+                assertThrows(BusinessException::class.java) {
+                    questParticipationService.participateQuest(questId, user)
+                }
+
+            // then
+            assertEquals(ErrorCode.ENTITY_NOT_FOUND, exception.errorCode)
+        }
+
+        @Test
+        fun `이미 참여한 퀘스트에 참여하면 예외가 발생한다`() {
+            // given
+            val user = User(
+                id = quest.user.id!! + 1,
+                socialId = "testSocialId",
+                nickname = "testNickname",
+                position = Position.BACKEND
+            )
+            every { questRepository.findByIdOrNull(any()) } returns quest
+            every { questParticipationRepository.existsById(any()) } returns true
+            every { questParticipationRepository.save(any()) } returns EntityFactory.testQuestParticipation()
+
+            // when
+            val exception =
+                assertThrows(BusinessException::class.java) {
+                    questParticipationService.participateQuest(questId, user)
+                }
+
+            // then
+            assertEquals(ErrorCode.ALREADY_PARTICIPATED_QUEST, exception.errorCode)
+        }
+
+        @Test
+        fun `퀘스트에 참여한다`() {
+            // given
+            val user = User(
+                id = quest.user.id!! + 1,
+                socialId = "testSocialId",
+                nickname = "testNickname",
+                position = Position.BACKEND
+            )
+            every { questRepository.findByIdOrNull(any()) } returns quest
+            every { questParticipationRepository.existsById(any()) } returns false
+            every { questParticipationRepository.save(any()) } returns EntityFactory.testQuestParticipation()
+
+            // when
+            questParticipationService.participateQuest(questId, user)
+
+            // then
+            verify { questParticipationRepository.save(any()) }
+        }
     }
 
-    @Test
-    fun `이미 참여한 퀘스트에 참여하면 예외가 발생한다`() {
-        // given
-        val user = User(
-            id = quest.user.id!! + 1,
-            socialId = "testSocialId",
-            nickname = "testNickname",
-            position = Position.BACKEND
-        )
-        every { questRepository.findByIdOrNull(any()) } returns quest
-        every { questParticipationRepository.existsById(any()) } returns true
-        every { questParticipationRepository.save(any()) } returns EntityFactory.testQuestParticipation()
+    @Nested
+    inner class QuestComplete {
 
-        // when
-        val exception =
-            assertThrows(BusinessException::class.java) {
-                questParticipationService.participateQuest(questId, user)
-            }
+        private val questParticipation = EntityFactory.testQuestParticipation()
 
-        // then
-        assertEquals(ErrorCode.ALREADY_PARTICIPATED_QUEST, exception.errorCode)
-    }
+        @Test
+        fun `참여하지 않은 퀘스트를 완료하면 예외가 발생한다`() {
+            // given
+            every { questParticipationRepository.findByIdOrNull(any()) } returns null
 
-    @Test
-    fun `퀘스트에 참여한다`() {
-        // given
-        val user = User(
-            id = quest.user.id!! + 1,
-            socialId = "testSocialId",
-            nickname = "testNickname",
-            position = Position.BACKEND
-        )
-        every { questRepository.findByIdOrNull(any()) } returns quest
-        every { questParticipationRepository.existsById(any()) } returns false
-        every { questParticipationRepository.save(any()) } returns EntityFactory.testQuestParticipation()
+            // when
+            val exception =
+                assertThrows(BusinessException::class.java) {
+                    questParticipationService.completeQuest(questId, user)
+                }
 
-        // when
-        questParticipationService.participateQuest(questId, user)
+            // then
+            assertEquals(ErrorCode.ENTITY_NOT_FOUND, exception.errorCode)
+        }
 
-        // then
-        verify { questParticipationRepository.save(any()) }
+        @Test
+        fun `이미 완료된 퀘스트를 완료하면 예외가 발생한다`() {
+            // given
+            every { questParticipationRepository.findByIdOrNull(any()) } returns questParticipation.also { it.isCompleted = true }
+
+            // when
+            val exception =
+                assertThrows(BusinessException::class.java) {
+                    questParticipationService.completeQuest(questId, user)
+                }
+
+            // then
+            assertEquals(ErrorCode.ALREADY_COMPLETED_QUEST, exception.errorCode)
+        }
+
+        @Test
+        fun `완료되지 않은 서브 퀘스트가 존재할 때 퀘스트를 완료하면 예외가 발생한다`() {
+            // given
+            every { questParticipationRepository.findByIdOrNull(any()) } returns questParticipation
+            every { subQuestService.countCompletedSubQuest(any(), any()) } returns 1
+
+            // when
+            val exception =
+                assertThrows(BusinessException::class.java) {
+                    questParticipationService.completeQuest(questId, user)
+                }
+
+            // then
+            assertEquals(ErrorCode.NOT_COMPLETED_QUEST, exception.errorCode)
+        }
+
+        @Test
+        fun `퀘스트를 완료한다`() {
+            // given
+            every { questParticipationRepository.findByIdOrNull(any()) } returns questParticipation.also { it.isCompleted = true }
+
+            // when
+            val exception =
+                assertThrows(BusinessException::class.java) {
+                    questParticipationService.completeQuest(questId, user)
+                }
+
+            // then
+            assertEquals(ErrorCode.ALREADY_COMPLETED_QUEST, exception.errorCode)
+        }
     }
 }
