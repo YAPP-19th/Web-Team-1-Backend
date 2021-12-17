985daa0a5ce96c94e0a9f824549c9bba7e7dce23
ayoung0073 <ayong0310@naver.com>
2021-12-18 02:07:11 +0900

    [GILJOB-98] 업적 등급 계산 로직 테스트 및 수정

--- src/main/kotlin/com/yapp/giljob/domain/user/application/AchieveCalculator.kt
+++ src/main/kotlin/com/yapp/giljob/domain/user/application/AchieveCalculator.kt
@@ -7,20 +7,20 @@ class AchieveCalculator {
     companion object {
         fun calculatePointAchieve(point: Long) =
             when (point) {
-                in PointAchieve.LEVEL_5..PointAchieve.LEVEL_4 -> 5
-                in PointAchieve.LEVEL_4..PointAchieve.LEVEL_3 -> 4
-                in PointAchieve.LEVEL_3..PointAchieve.LEVEL_2 -> 3
-                in PointAchieve.LEVEL_2..PointAchieve.LEVEL_1 -> 2
-                else -> 0
+                in PointAchieve.LEVEL_5 until PointAchieve.LEVEL_4 -> 5
+                in PointAchieve.LEVEL_4 until PointAchieve.LEVEL_3 -> 4
+                in PointAchieve.LEVEL_3 until PointAchieve.LEVEL_2 -> 3
+                in PointAchieve.LEVEL_2 until PointAchieve.LEVEL_1 -> 2
+                else -> 1
             }
 
         fun calculateQuestAchieve(questCount: Long) =
             when (questCount) {
-                in QuestAchieve.LEVEL_5..QuestAchieve.LEVEL_4 -> 5
-                in QuestAchieve.LEVEL_4..QuestAchieve.LEVEL_3 -> 4
-                in QuestAchieve.LEVEL_3..QuestAchieve.LEVEL_2 -> 3
-                in QuestAchieve.LEVEL_2..QuestAchieve.LEVEL_1 -> 2
-                else -> 0
+                in QuestAchieve.LEVEL_5 until QuestAchieve.LEVEL_4 -> 5
+                in QuestAchieve.LEVEL_4 until QuestAchieve.LEVEL_3 -> 4
+                in QuestAchieve.LEVEL_3 until QuestAchieve.LEVEL_2 -> 3
+                in QuestAchieve.LEVEL_2 until QuestAchieve.LEVEL_1 -> 2
+                else -> 1
             }
     }
 }
