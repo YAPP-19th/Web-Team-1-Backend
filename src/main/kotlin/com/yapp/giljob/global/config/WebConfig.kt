aed6445d520e283de50c60bb59655544ee3990c7
ayoung0073 <ayong0310@naver.com>
2021-12-18 15:19:02 +0900

    [GILJOB-116] cors 해결

---
+++ src/main/kotlin/com/yapp/giljob/global/config/WebConfig.kt
@@ -0,0 +1,19 @@
+package com.yapp.giljob.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods(
+                HttpMethod.GET.name,
+                HttpMethod.PATCH.name,
+                HttpMethod.POST.name
+            )
+    }
+}
