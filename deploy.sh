503be2021aabfcfb5467ea4d601588ba0a17089f
MoonAyoung <ayong0310@naver.com>
2021-12-18 19:22:56 +0900

    [GILJOB-117] jar 파일 경로 수정

--- deploy.sh
+++ deploy.sh
@@ -4,24 +4,26 @@ REPOSITORY=/home/ec2-user/giljob
 cd $REPOSITORY
 
 APP_NAME=giljob
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+JAR_NAME=$(ls $REPOSITORY/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/$JAR_NAME
+
+echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/deploy.log
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 종료할것 없음."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않음" >> /home/ec2-user/deploy.log
 else
-  echo "> kill -9 $CURRENT_PID"
+  echo "> kill -9 $CURRENT_PID" >> /home/ec2-user/deploy.log
   kill -9 $CURRENT_PID
   sleep 5
 fi
 
-echo "> $JAR_PATH 에 실행권한 추가"
+echo "> $JAR_PATH 에 실행 권한 추가" >> /home/ec2-user/deploy.log
 chmod +x $JAR_PATH
 
-echo "> $JAR_PATH 실행"
-nohup java -jar -Dspring.config.location=/home/ec2-user/application.yml $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+echo "> $JAR_PATH 실행" >> /home/ec2-user/deploy.log
+nohup java -jar -Dspring.config.location=/home/ec2-user/application.yml $JAR_PATH >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log
 
-echo "> 배포 완료"
+echo "> 배포 성공" >> /home/ec2-user/deploy.log
