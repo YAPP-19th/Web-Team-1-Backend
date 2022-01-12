681e0ad3cd7dbf44c223f88d26064812ba68b58a
ayoung0073 <ayong0310@naver.com>
2022-01-12 22:48:06 +0900

    [GILJOB-137] cors 전역 설정

--- src/main/kotlin/com/yapp/giljob/domain/quest/api/QuestController.kt
+++ src/main/kotlin/com/yapp/giljob/domain/quest/api/QuestController.kt
@@ -19,7 +19,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
-@CrossOrigin
 @RestController
 @RequestMapping("/api/quests")
 class QuestController(
--- src/main/kotlin/com/yapp/giljob/domain/user/api/UserRoadmapController.kt
+++ src/main/kotlin/com/yapp/giljob/domain/user/api/UserRoadmapController.kt
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
-@CrossOrigin
 @RestController
 @RequestMapping("/api/users/{userId}/roadmaps")
 class UserRoadmapController(
