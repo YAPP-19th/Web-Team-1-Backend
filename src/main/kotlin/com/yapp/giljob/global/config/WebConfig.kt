b99ff238cf5bd258518c48875f0833161d111bc1
ayoung0073 <ayong0310@naver.com>
2022-01-15 00:45:30 +0900

    [GILJOB-170] origin 허용 추가

--- src/main/kotlin/com/yapp/giljob/global/config/WebConfig.kt
+++ src/main/kotlin/com/yapp/giljob/global/config/WebConfig.kt
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://giljob.netlify.app", "http://localhost:5050")
+            .allowedOrigins("https://giljob.netlify.app", "http://www.giljob.co.kr", "http://localhost:5050")
             .allowedMethods(
                 HttpMethod.GET.name,
                 HttpMethod.PATCH.name,
